1ebf7488c0b61c3c3156833793099255e69247d9
Joyce <jycyeo@yahoo.com.sg>
2020-09-30 16:55:10 +0800

    add test unit

--- ghost_game.py
+++ ghost_game.py
@@ -1,44 +1,45 @@
 from enum import Enum
 from collections import deque
 
+
 class GhostGame:
 
     class GhostGameException(Exception):
         pass
 
     # parameters for validation
-    __MIN_PLAYERS = 3
+    __MIN_NUM_PLAYERS = 3
     __DEFAULT_NUM_PLAYERS = 7
-    __MAX_PLAYERS = 10
+    __MAX_NUM_PLAYERS = 10
 
     __MIN_WORD_LENGTH = 3
     __MAX_WORD_LENGTH = 15
 
     class __Roles(Enum):
-        GHOST   = 'Ghost'
-        TOWN    = 'Town'
-        FOOL    = 'Fool'
+        GHOST = 'Ghost'
+        TOWN = 'Town'
+        FOOL = 'Fool'
 
     # defines number of each role given number of players
     __ROLE_SETS = {
-        3   :   (1, 2, 0),
-        4   :   (1, 3, 0),
-        5   :   (2, 3, 0),
-        6   :   (2, 3, 1),
-        7   :   (2, 3, 2),
-        8   :   (2, 4, 2),
-        9   :   (3, 4, 2),
-        10  :   (3, 4, 3)
+        3: (1, 2, 0),
+        4: (1, 3, 0),
+        5: (2, 3, 0),
+        6: (2, 3, 1),
+        7: (2, 3, 2),
+        8: (2, 4, 2),
+        9: (3, 4, 2),
+        10: (3, 4, 3)
     }
 
     # fsm
     class __States(Enum):
-        SET_PARAMS          = 'Setting parameters'
-        REGISTER_PLAYERS    = 'Registering players'
-        GHOST_VOTE_ROUND    = 'Ghost Vote Round'
-        WORD_ROUND          = 'Word Round'
-        VOTE_ROUND          = 'Vote Round'
-        COMPLETE            = 'Complete'
+        SET_PARAMS = 'Setting parameters'
+        REGISTER_PLAYERS = 'Registering players'
+        GHOST_VOTE_ROUND = 'Ghost Vote Round'
+        WORD_ROUND = 'Word Round'
+        VOTE_ROUND = 'Vote Round'
+        COMPLETE = 'Complete'
 
     def __init__(self):
         self.__game_state = GhostGame.__States.SET_PARAMS
@@ -48,7 +49,7 @@ class GhostGame:
         self.__all_players = set()
         self.__alive_players = set()
 
-        self.__players_to_id = dict() # map from username to internal id
+        self.__players_to_id = dict()  # map from username to internal id
         self.__id_to_players = deque()
         self.__roles = list()
         self.__clues = list()
@@ -56,46 +57,68 @@ class GhostGame:
 
     def __check_game_state(self, expected_state: __States) -> None:
         if self.__game_state != expected_state:
-            raise GhostGameException(
-                'Invalid game state! Expected game to be in %s stage'
-                + ' but got %s stage' % (expected_state, self.game_state))
+            raise GhostGame.GhostGameException(
+                'Invalid game state! Expected game to be in %s stage but got '
+                '%s stage' % (expected_state, self.game_state)
+            )
 
     def set_param_num_players(self, value: int) -> None:
         self.__check_game_state(GhostGame.__States.SET_PARAMS)
-        if value < GhostGame.__MIN_PLAYERS or value > GhostGame.__MAX_PLAYERS:
-            raise GhostGameException(
-                'The game can only have 3 to 10 players')
+        if value < GhostGame.__MIN_NUM_PLAYERS or \
+                value > GhostGame.__MAX_NUM_PLAYERS:
+            raise GhostGame.GhostGameException(
+                'The game can only have 3 to 10 players'
+            )
         self.__num_players = value
 
     def set_param_town_word(self, value: str) -> None:
         self.__check_game_state(GhostGame.__States.SET_PARAMS)
+        if not value.isalpha():
+            raise GhostGame.GhostGameException(
+                'The word must be alphabetic (no numbers or symbols)'
+            )
         if len(value) < GhostGame.__MIN_WORD_LENGTH:
-            raise GhostGameException(
+            raise GhostGame.GhostGameException(
                 'The word cannot be too short (%d char min)' %
-                __MIN_WORD_LENGTH)
-        elif len(value) > GhostGame.__MAX_WORD_LENGTH:
-            raise GhostGameException(
+                GhostGame.__MIN_WORD_LENGTH
+            )
+        if len(value) > GhostGame.__MAX_WORD_LENGTH:
+            raise GhostGame.GhostGameException(
                 'The word cannot be too long (%d char max)' %
-                __MAX_WORD_LENGTH)
+                GhostGame.__MAX_WORD_LENGTH
+            )
 
         self.__town_word = value
 
     def set_param_fool_word(self, value: str) -> None:
         self.__check_game_state(GhostGame.__States.SET_PARAMS)
+        if not value.isalpha():
+            raise GhostGame.GhostGameException(
+                'The word must be alphabetic (no numbers or symbols)'
+            )
         if len(value) != len(self.__town_word):
-            raise GhostGameException(
-                'The fool word and town word must have the same length')
+            raise GhostGame.GhostGameException(
+                'The fool word and town word must have the same length. '
+                'Set the town word first.'
+            )
+        if value == self.__town_word:
+            raise GhostGame.GhostGameException(
+                'The fool word cannot be exactly the same as the town word.'
+            )
         self.__fool_word = value
 
-    def confirm_params_register_players(self) -> None:
+    def confirm_params_start_register(self) -> None:
         self.__check_game_state(GhostGame.__States.SET_PARAMS)
 
-        # set default number of players
-        self.set_param_num_players(GhostGame.__DEFAULT_NUM_PLAYERS)
+        # check other parameters
+        if self.__town_word is None:
+            raise GhostGame.GhostGameException('Town word has not been set')
+        if self.__fool_word is None:
+            raise GhostGame.GhostGameException('Fool word has not been set')
 
-        if self.__town_word is None or self.__fool_word is None:
-            raise GhostGameException(
-                'At least one of the words have not been set')
+        # set default number of players
+        if self.__num_players == 0:
+            self.set_param_num_players(GhostGame.__DEFAULT_NUM_PLAYERS)
 
         self.__id_to_players = [None] * self.__num_players
         self.__game_state = GhostGame.__States.REGISTER_PLAYERS
@@ -104,29 +127,30 @@ class GhostGame:
         self.__check_game_state(GhostGame.__States.REGISTER_PLAYERS)
 
         if username in self.__players_to_id:
-            raise GhostGameException(
+            raise GhostGame.GhostGameException(
                 'Player %s is already registered' % username)
 
         current_num_players = len(self.__all_players)
         if current_num_players >= self.__num_players:
-            raise GhostGameException(
+            raise GhostGame.GhostGameException(
                 'Player capacity of %d exceeded' % self.__num_players)
 
         player_id = current_num_players
-        self.__players_to_id[username] = player_id 
+        self.__players_to_id[username] = player_id
         self.__id_to_players.append(username)
         self.__all_players.add(player_id)
 
     def is_max_player_cap_reached(self) -> bool:
-        self.__check_game_state(GhostGame.__States.REGISTER_PlAYERS)
+        self.__check_game_state(GhostGame.__States.REGISTER_PLAYERS)
         return len(self.__all_players) >= self.__num_players
 
     def confirm_register_start_game(self) -> dict:
-        self.__check_game_state(GhostGame.__States.REGISTER_PlAYERS)
+        self.__check_game_state(GhostGame.__States.REGISTER_PLAYERS)
         if len(self.__all_players) < GhostGame.__MIN_NUM_PLAYERS:
-            raise GhostGameException(
+            raise GhostGame.GhostGameException(
                 'Not enough players have joined (min %d)' %
-                GhostGame.__MIN_NUM_PLAYERS)
+                GhostGame.__MIN_NUM_PLAYERS
+            )
 
         # update final player count
         self.__num_players = len(self.__all_players)
---
+++ ghost_game_tester.py
@@ -0,0 +1,27 @@
+from ghost_game import GhostGame
+import unittest
+
+
+class TestGhostGame(unittest.TestCase):
+
+    def test_param_example(self) -> GhostGame:
+        gg = GhostGame()
+        gg.set_param_num_players(3)
+        gg.set_param_town_word('egg')
+        gg.set_param_fool_word('pea')
+        gg.confirm_params_start_register()
+        return gg
+
+    def test_register_example(self) -> GhostGame:
+        gg = self.test_param_example()
+        gg.register_player('bacon')
+        gg.register_player('peanut')
+        self.assertFalse(gg.is_max_player_cap_reached())
+        gg.register_player('tomato')
+        self.assertTrue(gg.is_max_player_cap_reached())
+        gg.confirm_register_start_game()
+        return gg
+
+
+if __name__ == '__main__':
+    unittest.main()
